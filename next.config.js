1e6fa6d73eb35733fefc33a5f595f598747553cb
Alexsandro <sandro.alex_@hotmail.com>
2022-05-07 08:46:04 -0300

    feat: add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig}
+const nextConfig = {
+  reactStrictMode: true
+} */
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
+    // register: true,
+    // scope: '/app',
+    // sw: 'service-worker.js',
+    //...
+  }
+})
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,13 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/icon-512.png" alt="React Avançado" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+  </S.Wrapper>
 )
 
 export default Main
